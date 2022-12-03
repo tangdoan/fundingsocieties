9a092f38c43ee78361f5772be73eeefeb3aca74f
tang.doan <tdoan@squaretrade.com>
2022-12-04 00:29:02 +0700

    rm unnecessary file

--- PyTest/test_four.py
+++
@@ -1,4 +0,0 @@
-def test_response_200():
-    print("API working fine")
-def test_response_400():
-    print("API working fine")
--- PyTest/test_second.py
+++
@@ -1,6 +0,0 @@
-def test_login():
-    print("Login to application")
-def test_checkout():
-    print("Check out")
-def test_logout():
-    print("Logout from application")
