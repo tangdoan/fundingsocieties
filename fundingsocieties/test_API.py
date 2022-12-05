477d5edf00ee99eecdf78a3c40cfca783e7d2e70
tang.doan <tdoan@squaretrade.com>
2022-12-05 11:23:05 +0700

    add logging.info to API testing

--- fundingsocieties/test_API.py
+++ fundingsocieties/test_API.py
@@ -1,3 +1,4 @@
+import logging
 import random
 import time
 
@@ -11,18 +12,25 @@ ENDPOINT = "https://reqres.in"
 def test_get_list_users():
     response = requests.get(ENDPOINT + "/api/users?page=2")
     assert response.status_code == 200
+    logging.info("\nStatus code response: {}".format(response.status_code))
     data = response.json()
     per_page = data["per_page"]
     assert len(data) == per_page
+    logging.info("\nNumber of users listed per page: {}".format(per_page))
 
 
 @pytest.mark.API
 def test_get_single_user():
-    user_id = str(random.randint(1, 12))
+    user_id = random.randint(1, 12)
     response = requests.get(ENDPOINT + f"/api/users/{user_id}")
     assert response.status_code == 200
+    logging.info("\nStatus code response: {}".format(response.status_code))
     data = response.json()
-    assert str(data["data"]["id"]).replace('' '', '') == str(user_id)
+    assert data["data"]["id"] == user_id
+    logging.info("\nUser_id returned: {}".format(data["data"]["id"]))
+    logging.info("\nUser first name: {}".format(data["data"]["first_name"]))
+    logging.info("\nUser last name: {}".format(data["data"]["last_name"]))
+    logging.info("\nUser email: {}".format(data["data"]["email"]))
 
 
 @pytest.mark.API
@@ -30,15 +38,19 @@ def test_single_user_not_found():
     user_id = 23
     response = requests.get(ENDPOINT + f"/api/users/{user_id}")
     assert response.status_code == 404
+    logging.info("\nStatus code response: {}".format(response.status_code))
 
 
 @pytest.mark.API
 def test_list_resource():
     response = requests.get(ENDPOINT + "/api/unknown")
+    # Concern about API url: /unknown ?
     assert response.status_code == 200
+    logging.info("\nStatus code response: {}".format(response.status_code))
     data = response.json()
     per_page = data["per_page"]
     assert len(data) == per_page
+    logging.info("\nNumber of resource returned: {}".format(per_page))
 
 
 @pytest.mark.API
@@ -46,8 +58,11 @@ def test_single_resource():
     resource_id = random.randint(1, 6)
     response = requests.get(ENDPOINT + f"/api/unknown/{resource_id}")
     assert response.status_code == 200
+    logging.info("\nStatus code response: {}".format(response.status_code))
     data = response.json()
-    assert str(data["data"]["id"]).replace('' '', '') == str(resource_id)
+    assert data["data"]["id"] == resource_id
+    logging.info("\nResource id: {}".format(data["data"]["id"]))
+    logging.info("\nResource name: {}".format(data["data"]["name"]))
 
 
 @pytest.mark.API
@@ -55,6 +70,7 @@ def test_single_resource_not_found():
     resource_id = 23
     response = requests.get(ENDPOINT + f"/api/unknown/{resource_id}")
     assert response.status_code == 404
+    logging.info("\nStatus code response: {}".format(response.status_code))
 
 
 @pytest.mark.API
@@ -65,10 +81,13 @@ def test_create_user():
     }
     create_user_response = requests.post(ENDPOINT + "/api/users", json=payload)
     assert create_user_response.status_code == 201
+    logging.info("\nStatus code response: {}".format(create_user_response.status_code))
 
     data = create_user_response.json()
     assert str(payload["name"]) == data["name"]
+    logging.info("\nUser name: {}".format(data["name"]))
     assert str(payload["job"]) == data["job"]
+    logging.info("\nUser job: {}".format(data["job"]))
 
 
 # TBD: we have to get single user base on use_id after creating to make sure creation successfully
@@ -83,10 +102,15 @@ def test_update_user_using_put():
     user_id = 2
     update_user_response = requests.put(ENDPOINT + f"/api/users/{user_id}", json=payload)
     assert update_user_response.status_code == 200
+    logging.info("\nStatus code response: {}".format(update_user_response.status_code))
 
     data = update_user_response.json()
     assert str(payload["name"]) == data["name"]
+    logging.info("\nUser name: {}".format(data["name"]))
     assert str(payload["job"]) == data["job"]
+    logging.info("\nUser job: {}".format(data["job"]))
+
+# TBD: We have to get single user again to make sure we can update successfully
 
 
 @pytest.mark.API
@@ -98,10 +122,15 @@ def test_update_user_using_patch():
     user_id = 2
     update_user_response = requests.patch(ENDPOINT + f"/api/users/{user_id}", json=payload)
     assert update_user_response.status_code == 200
+    logging.info("\nStatus code response: {}".format(update_user_response.status_code))
 
     data = update_user_response.json()
     assert str(payload["name"]) == data["name"]
+    logging.info("\nUser name: {}".format(data["name"]))
     assert str(payload["job"]) == data["job"]
+    logging.info("\nUser job: {}".format(data["job"]))
+
+# TBD: We have to get single user again to make sure we can update successfully
 
 
 @pytest.mark.API
@@ -109,9 +138,10 @@ def test_delete_user():
     user_id = 2
     response = requests.delete(ENDPOINT + f"/api/users/{user_id}")
     assert response.status_code == 204
+    logging.info("\nStatus code response: {}".format(response.status_code))
 
 
-# TBD: We have to get single user again base on user_id after deleting for sure
+# TBD: We have to get single user again base on user_id after deleting
 
 
 @pytest.mark.API
@@ -122,6 +152,7 @@ def test_register_successful():
     }
     response = requests.post(ENDPOINT + "/api/register", json=payload)
     assert response.status_code == 200
+    logging.info("\nStatus code response: {}".format(response.status_code))
 
 
 @pytest.mark.API
@@ -131,9 +162,11 @@ def test_register_unsuccessful():
     }
     response = requests.post(ENDPOINT + "/api/register", json=payload)
     assert response.status_code == 400
+    logging.info("\nStatus code response: {}".format(response.status_code))
 
     data = response.json()
     assert data["error"] == "Missing password"
+    logging.info("\nError response: {}".format(data["error"]))
 
 
 @pytest.mark.API
@@ -144,6 +177,7 @@ def test_login_successful():
     }
     response = requests.post(ENDPOINT + "/api/login", json=payload)
     assert response.status_code == 200
+    logging.info("\nStatus code response: {}".format(response.status_code))
 
 
 @pytest.mark.API
@@ -153,8 +187,10 @@ def test_login_unsuccessful():
     }
     response = requests.post(ENDPOINT + "/api/login", json=payload)
     assert response.status_code == 400
+    logging.info("\nStatus code response: {}".format(response.status_code))
     data = response.json()
     assert data["error"] == "Missing password"
+    logging.info("\n Error response: {}".format(data["error"]))
 
 
 @pytest.mark.API
@@ -162,7 +198,9 @@ def test_delayed_response():
     response = requests.get(ENDPOINT + "/api/users?delay=3")
     time.sleep(3)
     assert response.status_code == 200
+    logging.info("\nStatus code response: {}".format(response.status_code))
 
     data = response.json()
     assert data["per_page"] == len(data)
+    logging.info("\nNumber of user response per page: {}".format(len(data)))
 
